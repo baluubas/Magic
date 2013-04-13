23023c108d9458b7deec17b639e1684e09d264c3
Anders Persson <anders@anderspersson.com>
2013-04-13 18:01:46 +0200

    Fixed bug that where preventing crashdump from being written

--- Source/Magic/App.xaml.cs
+++ Source/Magic/App.xaml.cs
@@ -15,7 +15,7 @@ namespace Magic
 
 		private void LogError(Exception exception)
 		{
-			var installDir = new DirectoryInfo(Assembly.GetEntryAssembly().Location).FullName;
+			var installDir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
 			var logFile = Path.Combine(installDir, "Crash.txt");
 			File.WriteAllText(logFile, exception.ToString());
 		}
